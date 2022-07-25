27ab6e48c23ff542d4aaee5c31550b2e0dcfa325
Denis Churkin <churkin@MacBook-Pro-Denis.local>
2022-07-25 18:00:16 +0300

    add files

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Content from './components/Profile';
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+
+
+const App = () => {
+  return (
+    <div className="app-wrapper">
+      <Header />
+      <Navbar />
+      <Content />
+    </div>
+  );
+}
+
+
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+
+const Navbar = () => {
+    return (
+        <nav className="nav">
+            <a href="">
+                <div>Profile</div>
+            </a>
+            <a href="">
+                <div>Messages</div>
+            </a>
+            <a href="">
+                <div>News</div>
+            </a>
+            <a href="">
+                <div>Music</div>
+            </a>
+            <a href="">
+                <div>Settings</div>
+            </a>
+
+        </nav>
+    );
+}
+export default Navbar;
